c456fab1fdf8f16312d7d2558681eafe1a2eb3ab
Luke Titley <luke@titley.com>
2021-01-20 21:44:56 +0100

    Added a header file with the basic annotations attributes in it

---
+++ bind/bind_defs.hpp
@@ -0,0 +1,4 @@
+#define CPPMM_IGNORE __attribute__((annotate("cppmm:ignore")))
+#define CPPMM_RENAME(x) __attribute__((annotate("cppmm:rename:" #x)))
+#define CPPMM_OPAQUEBYTES __attribute__((annotate("cppmm:opaquebytes")))
+#define CPPMM_VALUETYPE __attribute__((annotate("cppmm:valuetype")))
