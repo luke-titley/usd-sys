9c24e618dcf84c2408d82a91e34fe1c6809add29
Luke Titley <luke@titley.com>
2021-01-20 22:11:40 +0100

    Call cmake from the bind script

--- bind.sh
+++ bind.sh
@@ -12,4 +12,5 @@ ${CPPMM} ../bind -u         \
     -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
     -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
     -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+cmake .
 popd
