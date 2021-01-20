e31eef43f0daca0f84fb3361a4fb5d803c31ce1d
Luke Titley <luke@titley.com>
2021-01-20 21:42:49 +0100

    Added a bind.sh file which will invoke cppmm to generate bindings

---
+++ bind.sh
@@ -0,0 +1,15 @@
+CPPMM=../../cppmm/build/cppmm
+
+mkdir -p build
+rm -rf build/*
+
+pushd build
+${CPPMM} ../bind -u         \
+    -o .                    \
+    -i $PWD/../bind/        \
+    --                      \
+    -I thirdparty/include/           \
+    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
+    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
+    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+popd
