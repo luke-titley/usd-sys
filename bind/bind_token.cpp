1d95f650f9b4692e555782072bb89cbdd7a30bb8
Luke Titley <luke@titley.com>
2021-01-20 22:18:20 +0100

    Renamed bind_token.hpp to bind_token.cpp and got a simple tf token wrapper generating

---
+++ bind/bind_token.cpp
@@ -0,0 +1,35 @@
+#include "bind_defs.hpp"
+
+#include "pxr/base/tf/token.h"
+
+namespace cppmm_bind {
+
+namespace PXR_INTERNAL_NS {
+
+namespace pxr = ::PXR_INTERNAL_NS;
+
+class TfToken {
+    TfToken(pxr::TfToken const& rhs) CPPMM_RENAME(copy);
+    pxr::TfToken& operator= (pxr::TfToken const& rhs) CPPMM_RENAME(assign);
+    ~TfToken() CPPMM_RENAME(destruct);
+
+    explicit TfToken(char const* s) CPPMM_RENAME(from_string);
+
+    size_t Hash() const;
+    size_t size() const;
+    char const* GetText() const;
+    char const *data() const;
+    bool operator==(pxr::TfToken const& o) const;
+    bool operator!=(pxr::TfToken const& o) const;
+    bool operator==(const char * o) const CPPMM_RENAME(eq_string);
+    bool operator!=(const char * o) const CPPMM_RENAME(neq_string);
+    bool operator<(pxr::TfToken const& r) const;
+
+    bool IsEmpty() const;
+    bool IsImmortal() const;
+
+} CPPMM_OPAQUEBYTES;
+
+}
+
+}
--- bind/bind_token.hpp
+++
@@ -1,7 +0,0 @@
-#include "bind_defs.hpp"
-
-#include "pxr/base/tf/token.h"
-
-namespace cppmm_bind {
-
-}
