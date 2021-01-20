594279a000257bc3aff6b5103d2e55e9c98d6746
Luke Titley <luke@titley.com>
2021-01-20 21:45:45 +0100

    Forgot pragma once

--- bind/bind_defs.hpp
+++ bind/bind_defs.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #define CPPMM_IGNORE __attribute__((annotate("cppmm:ignore")))
 #define CPPMM_RENAME(x) __attribute__((annotate("cppmm:rename:" #x)))
 #define CPPMM_OPAQUEBYTES __attribute__((annotate("cppmm:opaquebytes")))
