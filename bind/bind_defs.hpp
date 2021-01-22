fb11782cb2377f43e86ec12ea033412a3e5627b6
Luke Titley <luke@titley.com>
2021-01-22 13:46:39 +0100

    Deleting binding files. Try one simple bind.hpp file

--- bind/bind_defs.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define CPPMM_IGNORE __attribute__((annotate("cppmm:ignore")))
-#define CPPMM_RENAME(x) __attribute__((annotate("cppmm:rename:" #x)))
-#define CPPMM_OPAQUEBYTES __attribute__((annotate("cppmm:opaquebytes")))
-#define CPPMM_VALUETYPE __attribute__((annotate("cppmm:valuetype")))
