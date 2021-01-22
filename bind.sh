3191fa91384d381e51185faf9272913db46336bf
Luke Titley <luke@titley.com>
2021-01-22 13:48:13 +0100

    Switch over bind.sh to using castxml

--- bind.sh
+++ bind.sh
@@ -1,17 +1,18 @@
-CPPMM=../../cppmm/build/cppmm
+CASTXML=$PWD/castxml/bin/castxml
 
 mkdir -p build
 rm -rf build/*
 
 pushd build
-${CPPMM} ../bind -u         \
-    -o .                    \
-    -i $PWD/../bind/        \
-    -i $PWD/../thirdparty/include  \
-    --                      \
-    -I ../thirdparty/include/           \
-    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
-    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
-    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
-cmake .
+#${CPPMM} ../bind -u         \
+#    -o .                    \
+#    -i $PWD/../bind/        \
+#    -i $PWD/../thirdparty/include  \
+#    --                      \
+#    -I ../thirdparty/include/           \
+#    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
+#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
+#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+#cmake .
+${CASTXML}
 popd
